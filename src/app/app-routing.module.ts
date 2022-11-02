d3b327380352b8efecbd98432db79d4525a20554
The Buildbot <buildbot@angular2buch.de>
2022-11-02 06:59:20 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/98b792b24c185559b625c28e65e70c2f4c965af2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { AuthGuard } from './shared/auth.guard';
+import { authGuard } from './shared/auth.guard';
 
 const routes: Routes = [
   {
@@ -21,7 +21,7 @@ const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
-    canActivate: [AuthGuard]
+    canActivate: [authGuard]
   }
 ];
 
