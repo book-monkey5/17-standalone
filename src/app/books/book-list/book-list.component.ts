aed23c401be09220b074a9c92b1e6f3dddeb8644
The Buildbot <buildbot@angular2buch.de>
2022-11-09 15:09:18 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/7ed3c3efbec2f07846b9b9768f40ee9da00e7d20

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,6 +1,8 @@
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
+import { Book } from '../../shared/book';
 import { BookStoreService } from '../../shared/book-store.service';
 import { BookListItemComponent } from '../book-list-item/book-list-item.component';
 
@@ -15,7 +17,9 @@ import { BookListItemComponent } from '../book-list-item/book-list-item.componen
   ]
 })
 export class BookListComponent {
-  books$ = this.service.getAll();
+  books$: Observable<Book[]>;
 
-  constructor(private service: BookStoreService) {}
+  constructor(private service: BookStoreService) {
+    this.books$ = this.service.getAll();
+  }
 }
